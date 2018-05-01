4981705ebf1f7600bde4a2287f46569a1c986214
PDHS lab <pdhslab@DSUSD.K12.CA.US>
2018-05-01 10:45:18 -0700

    finished program, added exit function, start function fixed, added dictionary.

--- RSAproject.py
+++ RSAproject.py
@@ -4,10 +4,8 @@ n=0
 d=0        
 decrypted_message= ""
 
-#def start():
-  #print("Welcome to the Cool™ Encryption and Decryption Program [With cool exit feature!]")
-  #whatdo = input("Please select what you'd like to do: encrypt, decrypt, or exit.")
-  #while whatdo != "exit"
+endictcryption  = dict()
+dictcryption = dict()
 
 def encrypt(e, n):
     encrypted_message= ""
@@ -16,13 +14,17 @@ def encrypt(e, n):
     n= int(input('Please enter a value for n:'))
     print("n =", n)
     message= input("Please enter the message you would like to encrypt:")
-    for x in message:        
+    for x in message:
+      if x in endictcryption:
+        encrypted_message += endictcryption[x]
+      else:
         numerize= ord(x)
         encrypt = pow(numerize, e, n)
         denumerize = chr(encrypt) 
-        encrypted_message += denumerize   
+        encrypted_message += denumerize 
+        endictcryption[x] = denumerize
     print ("Your encrypted message is:", encrypted_message)
-    
+  
 def decrypt(d, n):
     decrypted_message= ""
     d= int(input('Please enter a value in for d:'))
@@ -30,9 +32,28 @@ def decrypt(d, n):
     n= int(input('Please enter a value for n:'))
     print("n =", n)
     encrypted_message= input("Please enter the message you would like to decrypt:")
-    for t in encrypted_message:        
+    for t in encrypted_message:
+      if t in dictcryption:
+        decrypted_message += dictcryption[t]
+    else:
       numerize = ord(t)
       decrypt = pow(numerize, d, n)
       denumerize = chr(decrypt)
       decrypted_message += denumerize
+      dictcryption[t] = denumerize
     print ("Your decrypted message is:", decrypted_message)
+
+def start():
+  whatdo = 0
+  print("Welcome to the Cool™ Encryption and Decryption Program [With cool exit feature!]")
+  command = input("Please select what you'd like to do: encrypt, decrypt, or exit.")
+  while (whatdo != 1):
+    if command == "encrypt" or command == "Encrypt":
+      encrypt(e, n)
+    elif command == "decrypt" or command == "Decrypt":
+      decrypt(d, n)
+    elif command == "exit" or command == "Exit":
+      whatdo = 1
+    else:
+      print ("That is incorrect! Please write in a proper answer.")
+  print ("Thank you for using our program, have a great day!")
